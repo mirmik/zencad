26733df508b4b85dec9e476acbdc5c1f58fa07fa
mirmik <mirmikns@yandex.ru>
2018-03-10 03:10:16 +0300

    a

--- make.py
+++ make.py
@@ -7,6 +7,8 @@ import licant
 import licant.libs
 
 qt_include_path = "~/Qt/5.10.1/gcc_64/include/"
+qt_lib_path = "/home/mirmik/Qt/5.10.1/gcc_64/lib/"
+boost_lib_path = "./third-party/"
 python_include_path = "/usr/include/python3.5m/"
 
 licant.libs.include("gxx")
@@ -20,13 +22,13 @@ module('libraries',
         python_include_path,
     ],
     
-    cxx_flags = '-fPIC -L/home/mirmik/Qt/5.10.1/gcc_64/include/QtCore -DQT_NO_VERSION_TAGGING',
+    cxx_flags = '-fPIC -L/home/mirmik/Qt/5.10.1/gcc_64/include/QtCore -L./third-party -DQT_NO_VERSION_TAGGING',
     cc_flags = '-fPIC',
 
     libs = [
         'Qt5Core', 
         'Qt5Widgets', 
-        #'Qt5Test', 
+        'Qt5Test', 
         'Qt5Gui', 
         'Qt5OpenGL',
 
@@ -50,6 +52,11 @@ module('libraries',
         'boost_python-py35',
         'boost_system',
     ],
+
+    #objects = [
+    #	"/usr/lib/x86_64-linux-gnu/libboost_python-py35.a",
+    #	 qt_lib_path + "libQt5Core.la",
+    #],
 
     include_modules = [
         submodule("gxx.print", "cout"),
