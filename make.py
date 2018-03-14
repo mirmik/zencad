5741a404ecd980149531af020504f4be3da29eec
mirmik <mirmikns@yandex.ru>
2018-03-14 11:35:07 +0300

    multiversion

--- make.py
+++ make.py
@@ -6,33 +6,15 @@ from licant.modules import submodule, module
 import licant
 import licant.libs
 
-qt_include_path = "/usr/include/x86_64-linux-gnu/qt5/"
-#qt_lib_path = "/home/mirmik/Qt/5.10.1/gcc_64/lib/"
-boost_lib_path = "./third-party/"
-python_include_path = "/usr/include/python3.5/"
-
 licant.libs.include("gxx")
 
-module('libraries', 
-    include_paths = [
-       '/usr/include/oce',
-       './src',
-        "./",
-        qt_include_path,
-        python_include_path,
-    ],
-    
-    cxx_flags = '-fPIC -DQT_NO_VERSION_TAGGING',
-    cc_flags = '-fPIC',
+libqt_include_path = "/usr/include/x86_64-linux-gnu/qt5/"
+liboce_include_path = "/usr/include/oce"
+python_include_prefix = "/usr/include/"
 
-    libs = [
-        'Qt5Core', 
-        'Qt5Widgets', 
-        'Qt5Test', 
-        'Qt5Gui', 
-        'Qt5OpenGL',
-
-        'TKernel',
+module('liboce', 
+	libs = [     
+		'TKernel',
         'TKMath',
         'TKG3d',
         'TKBRep',
@@ -50,32 +32,52 @@ module('libraries',
         'TKSTL',
         'TKBin',
     ],
-
-    include_modules = [
-        submodule("gxx.print", "cout"),
-        submodule("gxx.dprint", "cout"),
-        submodule("gxx", "posix"),
-        submodule("gxx.include"),
-    ], 
+    include_paths = [liboce_include_path]    
 )
 
-shared_library("zenlib",
-    target = "zencad/zenlib.so",
-    srcdir = "src",
-    sources = [
-        "zencad/base.cpp", 
-        "zencad/solid.cpp", 
-        "zencad/wire.cpp", 
-        "zencad/boolops.cpp", 
-        "zencad/cache.cpp", 
-        "zencad/pywrap.cpp", 
-        "zencad/trans.cpp", 
-        "zencad/topo.cpp",
-        "zencad/ZenWidget.cpp", 
-        "zencad/DisplayWidget.cpp", 
-        "zencad/widget.cpp",
+module('libqt', 
+	libs = [    
+        'Qt5Core', 
+        'Qt5Widgets', 
+        'Qt5Test', 
+        'Qt5Gui', 
+        'Qt5OpenGL',
     ],
-    moc = ["zencad/DisplayWidget.h", "zencad/ZenWidget.h"],    
-    include_modules = [submodule("libraries")],
+    include_paths = [libqt_include_path]    
 )
-doit("zenlib")
+
+def registry_library(py):
+	shared_library("zenlib." + py,
+	    target = "zencad/" + py + "/zenlib.so",
+	    builddir = "build/" + py,
+	    
+	    cxx_flags = '-fPIC -DQT_NO_VERSION_TAGGING',
+	    cc_flags = '-fPIC',
+	
+	    srcdir = "src",
+	    sources = [
+	        "zencad/base.cpp", 
+	        "zencad/solid.cpp", 
+	        "zencad/wire.cpp", 
+	        "zencad/boolops.cpp", 
+	        "zencad/cache.cpp", 
+	        "zencad/pywrap.cpp", 
+	        "zencad/trans.cpp", 
+	        "zencad/topo.cpp",
+	        "zencad/ZenWidget.cpp", 
+	        "zencad/DisplayWidget.cpp", 
+	        "zencad/widget.cpp",
+	    ],
+	    moc = ["zencad/DisplayWidget.h", "zencad/ZenWidget.h"],    
+	    include_modules = [submodule("libqt"), submodule("liboce"),
+	    	submodule("gxx", "posix")],
+	    include_paths = [".", "src", python_include_prefix + py]
+	)
+
+registry_library("python2.7")
+registry_library("python3.5")
+registry_library("python3.5m")
+
+doit("zenlib.python2.7")
+doit("zenlib.python3.5")
+doit("zenlib.python3.5m")
