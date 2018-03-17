676ac9724ffc17c7b4e6e0fbccf11d90dfed843d
mirmik <mirmikns@yandex.ru>
2018-03-17 18:10:18 +0300

    a
    Former-commit-id: 957d6274dfc7420b15edd165b8f4e30a78ad2827

--- zencad/face.py
+++ zencad/face.py
@@ -1,2 +1,26 @@
 from zencad.zenlib import face_polygon as polygon
 from zencad.zenlib import face_circle as circle
+
+import math
+import zencad.math3
+from zencad import error
+
+def ngon(n, rad = None, a = None):
+	if a == None and rad == None:
+		zencad.error("ngon args error")
+
+	angles = [2*math.pi / n * i for i in range(0, n)]
+
+	if a != None:
+		rad = a / 2 / math.sin(math.pi/n)
+
+	pnts = zencad.math3.points((rad*math.cos(a), rad*math.sin(a)) for a in angles)
+	return polygon(pnts)	
+
+
+
+	
+
+def square(a, center = False):
+	if center:
+		return ngon(n = 4, a = a)
--- zencad/solid.py
+++ zencad/solid.py
@@ -3,4 +3,10 @@ from zencad.zenlib import solid_sphere as sphere
 from zencad.zenlib import solid_cylinder as cylinder
 from zencad.zenlib import solid_torus as torus
 from zencad.zenlib import solid_load as load
-from zencad.zenlib import solid_linear_extrude as linear_extrude
+
+from zencad.zenlib import solid_linear_extrude as __linear_extrude
+
+import zencad.math3
+
+def linear_extrude(face, vector):
+	return __linear_extrude(face, zencad.math3.vector(*vector))
