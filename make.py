29dd617cda32c010031247b862bf8222d6511143
mirmik <mirmikns@yandex.ru>
2018-03-20 10:34:42 +0300

    automate setup
    Former-commit-id: 962b5237d3fa3fd2e0e82c011761f33ec23478ad

--- make.py
+++ make.py
@@ -15,98 +15,124 @@ python_include_prefix = "/usr/include/"
 module('liboce', 
 	libs = [     
 		'TKernel',
-        'TKMath',
-        'TKG3d',
-        'TKBRep',
-        'TKGeomBase',
-        'TKGeomAlgo',
-        'TKTopAlgo',
-        'TKPrim',
-        'TKBO',
-        'TKBool',
-        'TKOffset',
-        'TKService',
-        'TKV3d',
-        'TKOpenGl',
-        'TKFillet',
-        'TKSTL',
-        'TKBin',
-    ],
-    include_paths = [liboce_include_path]    
+		'TKMath',
+		'TKG3d',
+		'TKBRep',
+		'TKGeomBase',
+		'TKGeomAlgo',
+		'TKTopAlgo',
+		'TKPrim',
+		'TKBO',
+		'TKBool',
+		'TKOffset',
+		'TKService',
+		'TKV3d',
+		'TKOpenGl',
+		'TKFillet',
+		'TKSTL',
+		'TKBin',
+	],
+	include_paths = [liboce_include_path]    
 )
 
 module('libqt', 
 	libs = [    
-        'Qt5Core', 
-        'Qt5Widgets', 
-        'Qt5Test', 
-        'Qt5Gui', 
-        'Qt5OpenGL',
-    ],
-    include_paths = [libqt_include_path]    
+		'Qt5Core', 
+		'Qt5Widgets', 
+		'Qt5Test', 
+		'Qt5Gui', 
+		'Qt5OpenGL',
+	],
+	include_paths = [libqt_include_path]    
 )
 
 def registry_library(py):
 	shared_library("zenlib." + py,
-	    target = "zencad/" + py + "/zenlib.so",
-	    builddir = "build/" + py,
-	    
-	    cxx_flags = '-fPIC -DQT_NO_VERSION_TAGGING',
-	    cc_flags = '-fPIC',
+		target = "zencad/" + py + "/zenlib.so",
+		builddir = "build/" + py,
+		
+		cxx_flags = '-fPIC -DQT_NO_VERSION_TAGGING',
+		cc_flags = '-fPIC',
 	
-	    srcdir = "src",
-	    sources = [
-	        "zencad/base.cpp", 
-	        "zencad/solid.cpp", 
-	        "zencad/wire.cpp", 
-	        "zencad/boolops.cpp", 
-	        "zencad/cache.cpp", 
-	        "zencad/pywrap.cpp", 
-	        "zencad/trans.cpp", 
-	        "zencad/topo.cpp",
-            "zencad/face.cpp",
-	        "zencad/ZenWidget.cpp", 
-	        "zencad/DisplayWidget.cpp", 
-	        "zencad/widget.cpp",
-	    ],
-	    moc = ["zencad/DisplayWidget.h", "zencad/ZenWidget.h"],    
-	    include_modules = [
-            "libqt", 
-            "liboce",
-	    	("gxx", "posix"),
-            ("gxx.print", "cout"),
-            ("gxx.dprint", "cout")
-        ],
-	    include_paths = [
-            ".", 
-            "src", 
-            python_include_prefix + py
-        ]
+		srcdir = "src",
+		sources = [
+			"zencad/base.cpp", 
+			"zencad/solid.cpp", 
+			"zencad/wire.cpp", 
+			"zencad/boolops.cpp", 
+			"zencad/cache.cpp", 
+			"zencad/pywrap.cpp", 
+			"zencad/trans.cpp", 
+			"zencad/topo.cpp",
+			"zencad/face.cpp",
+			"zencad/ZenWidget.cpp", 
+			"zencad/DisplayWidget.cpp", 
+			"zencad/widget.cpp",
+		],
+		moc = ["zencad/DisplayWidget.h", "zencad/ZenWidget.h"],    
+		include_modules = [
+			"libqt", 
+			"liboce",
+			("gxx", "posix"),
+			("gxx.print", "cout"),
+			("gxx.dprint", "cout")
+		],
+		include_paths = [
+			".", 
+			"src", 
+			python_include_prefix + py
+		]
 	)
 
 registry_library("python2.7")
 registry_library("python3.5")
-registry_library("python3.5m")
+#registry_library("python3.5m")
+registry_library("python3.6")
 
 @licant.routine
 def local35():
-    licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5/zenlib.so")
-    licant.do("zencad/zenlib.so", "makefile")
+	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5/zenlib.so")
+	licant.do("zencad/zenlib.so", "makefile")
 
 @licant.routine
-def local35m():
-    licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5m/zenlib.so")
-    licant.do("zencad/zenlib.so", "makefile")
+def local36():
+	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.6/zenlib.so")
+	licant.do("zencad/zenlib.so", "makefile")
+
+#@licant.routine
+#def local35m():
+#	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5m/zenlib.so")
+#	licant.do("zencad/zenlib.so", "makefile")
 
 @licant.routine
 def local27():
-    licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python2.7/zenlib.so")
-    licant.do("zencad/zenlib.so", "makefile")
+	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python2.7/zenlib.so")
+	licant.do("zencad/zenlib.so", "makefile")
 
 licant.add_makefile_target(tgt = "all", targets = [
-    "zenlib.python2.7",
-    "zenlib.python3.5",
-    "zenlib.python3.5m",
+	"zenlib.python2.7",
+	"zenlib.python3.5",
+	"zenlib.python3.6",
 ])
+
+import os
+@licant.routine
+def wheels():
+	licant.do("all", "makefile")
+
+	def do_for_suffix(suffx):
+		os.system("cp zencad/python{}/zenlib.so zencad/zenlib.so".format(suffx))
+		os.system("python{} setup.py bdist_wheel".format(suffx))
+
+	do_for_suffix("2.7")
+	do_for_suffix("3.5")
+	do_for_suffix("3.6")
+
+
+@licant.routine
+def publish():
+	licant.do("all", "makefile")
+	licant.do("wheels")
+	os.system("twine upload dist/* --repository-url https://upload.pypi.org/legacy/")
 
 licant.ex(default = "all")
