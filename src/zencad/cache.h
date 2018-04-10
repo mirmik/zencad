fa0d66d248c8f11e97b7635bfe6566aa00f68cfa
mirmik <mirmikns@yandex.ru>
2018-04-10 08:29:22 +0300

    a

--- src/zencad/cache.h
+++ src/zencad/cache.h
@@ -4,11 +4,15 @@
 #include <string>
 #include <set>
 
+#include <zencad/base.h>
+
 extern std::set<std::string> zencache_set;
 
 bool zencache_is_enabled();
 void zencache_enable(const std::string& path);
 void zencache_disable();
 std::string zencache_path();
+
+//std::string check_cache(ZenCadObject& zobj);
 
 #endif
