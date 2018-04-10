2a9567c760bc9c6cc4fd56905d04b8231ac5222d
mirmik <mirmikns@yandex.ru>
2018-04-10 13:00:07 +0300

    a
    Former-commit-id: 345698cded95a2c100cc975d5b365c84e539e4b7

--- test/solid/box.py
+++ test/solid/box.py
@@ -6,11 +6,7 @@ sys.path.insert(0, "../..")
 
 import zencad
 
-m0 = zencad.box(size = [20, 20, 10], center = True)
-m1 = zencad.box(size = 20, center = True)
-m2 = zencad.box(size = 20)
+m = zencad.box(size = [20, 20, 10], center = True)
+zencad.display(m)
 
-zencad.display(m0)
-zencad.display(m1.right(30))
-zencad.display(m2.left(30))
 zencad.show()
--- test/solid/cylinder.py
+++ test/solid/cylinder.py
@@ -6,8 +6,6 @@ sys.path.insert(0, "../..")
 
 import zencad
 m0 = zencad.cylinder(r = 10, h = 20)
-m1 = zencad.cylinder(10, 20, True)
 
 zencad.display(m0)
-zencad.display(m1.right(30))
 zencad.show()
