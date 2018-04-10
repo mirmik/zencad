345698cded95a2c100cc975d5b365c84e539e4b7
mirmik <mirmikns@yandex.ru>
2018-04-10 13:00:07 +0300

    a

--- test/solid/cone.py
+++ test/solid/cone.py
@@ -5,9 +5,7 @@ import sys
 sys.path.insert(0, "../..")
 
 import zencad
-m0 = zencad.cone(r1 = 10, r2 = 0, h = 20)
-m1 = zencad.cone(r1 = 10, r2 = 20, h = 20, center = True)
+m = zencad.cone(r1 = 20, r2 = 10, h = 20, center = True)
 
-zencad.display(m0)
-zencad.display(m1.right(30))
+zencad.display(m)
 zencad.show()
