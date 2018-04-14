cdd1cc7478e8a6c474ac8728a53d0e88f87bdde0
mirmik <mirmikns@yandex.ru>
2018-04-14 20:03:33 +0300

    wire interpolate

--- src/zencad/math3.h
+++ src/zencad/math3.h
@@ -21,14 +21,31 @@ struct ZenDirection3 : public XYZ {
 };
 */
 
-struct ZenVector3 : public XYZ {
+/*struct ZenVector3 : public XYZ {
 	ZenVector3(double x, double y, double z) : XYZ(x,y,z) {}
 	gp_Vec Vec() { return gp_Vec(x,y,z); }	
 };
 
 static inline void operator&(ZenVisitor& v, ZenVector3& vec) {
 	v&vec.x; v&vec.y; v&vec.z;
-}
+}*/
+
+struct ZenVector3 : public ZenCadObject {
+	double x, y, z;
+	
+	const char* class_name() const override { return "ZenVector3"; }
+	ZenVector3(double x, double y) : x(x), y(y), z(0) { initialize_hash(); }
+	ZenVector3(double x, double y, double z) : x(x), y(y), z(z) { initialize_hash(); }
+	gp_Vec Vec() { return gp_Vec(x,y,z); }
+	void vreflect(ZenVisitor& v) override { v&x; v&y; v&z; }
+
+	void prepare() override {
+		//m_native = //BRepBuilderAPI_MakeVertex(Pnt()).Vertex();
+	};
+
+	bool operator == (const ZenVector3& oth) { return oth.x == x && oth.y == y && oth.z == z; }
+	bool operator != (const ZenVector3& oth) { return ! operator==(oth); }
+};
 
 /*struct ZenVector3 : public ZenCadObject{
 	double x, y, z;
--- zencad/wire.py
+++ zencad/wire.py
@@ -2,5 +2,15 @@ from zencad.zenlib import wire_segment as segment
 from zencad.zenlib import wire_polysegment as polysegment
 from zencad.zenlib import wire_circle_arc_by_points as arc_by_points
 from zencad.zenlib import wire_circle as circle
+
+from zencad.zenlib import wire_interpolate as interpolate
+
+import zencad.math3
+
 #from zencad.zenlib import wire_complex as make_wire
 #import zencad.math3#
+
+#def interpolate(pnts, tan closed = False):
+	#parr = [ l[0] if isinstance(l, tuple) else l for l in lst ]
+	#tarr = [ l[1] if isinstance(l, tuple) else zencad.math3.vector(0,0,0) for l in lst ]
+#	return _interpolate(parr, tarr, closed)
