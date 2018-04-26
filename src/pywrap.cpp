8a3687ccd992eba8db679aab2695ef6bc44fac86
mirmik <mirmikns@yandex.ru>
2018-04-27 01:40:03 +0300

    a

--- src/pywrap.cpp
+++ src/pywrap.cpp
@@ -8,7 +8,8 @@ namespace py = pybind11;
 
 #define DEF_TRANSFORM_OPERATIONS(TYPE) 				\
 .def("transform", &TYPE::transform)				\
-.def("translate", &TYPE::translate)				\
+.def("translate", (TYPE(TYPE::*)(double, double, double))&TYPE::transform)				\
+.def("translate", (TYPE(TYPE::*)(double, double))&TYPE::translate)				\
 .def("up", &TYPE::up)							\
 .def("down", &TYPE::down)						\
 .def("right", &TYPE::right)						\
@@ -35,6 +36,7 @@ PYBIND11_MODULE(zenlib, m) {
 	//py::register_exception<servoce::RuntimeException>(m, "ServoceRuntimeException");
 
 	py::class_<servoce::point3>(m, "point3")
+		DEF_TRANSFORM_OPERATIONS(servoce::point3)
 		.def(py::init<double,double,double>())
 		.def(py::init<double,double>())
 		.def_readwrite("x", &servoce::point3::x)
@@ -43,6 +45,7 @@ PYBIND11_MODULE(zenlib, m) {
 	;
 	
 	py::class_<servoce::vector3>(m, "vector3")
+		DEF_TRANSFORM_OPERATIONS(servoce::vector3)
 		.def(py::init<double,double,double>())
 		.def(py::init<double,double>())
 		.def_readwrite("x", &servoce::vector3::x)
@@ -66,7 +69,7 @@ PYBIND11_MODULE(zenlib, m) {
 	m.def("make_torus", 	servoce::prim3d::make_torus, py::arg("r1"), py::arg("r2"));
 
 	m.def("make_linear_extrude", 	servoce::sweep3d::make_linear_extrude, py::arg("shp"), py::arg("vec"));
-	m.def("make_pipe", 				servoce::sweep3d::make_pipe, py::arg("profile"), py::arg("path"));
+	m.def("make_pipe", 				servoce::sweep3d::make_pipe, py::arg("prof"), py::arg("path"));
 
 	py::class_<servoce::face, servoce::shape>(m, "Face")
 		DEF_TRANSFORM_OPERATIONS(servoce::face)
@@ -80,19 +83,23 @@ PYBIND11_MODULE(zenlib, m) {
 	m.def("make_square", 	servoce::prim2d::make_square, py::arg("a"), py::arg("center") = false);
 	m.def("make_rectangle", servoce::prim2d::make_rectangle, py::arg("a"), py::arg("b"), py::arg("center") = false);
 	m.def("make_polygon", 	(servoce::face(*)(const std::vector<servoce::point3>&))&servoce::prim2d::make_polygon, py::arg("pnts"));
-
+	m.def("make_sweep", 	servoce::sweep2d::make_sweep, py::arg("prof"), py::arg("path"));
 
 	py::class_<servoce::wire, servoce::shape>(m, "Wire")
 		DEF_TRANSFORM_OPERATIONS(servoce::wire)
+		.def("face", &servoce::wire::to_face)
 	;
 	
 	m.def("make_segment", servoce::curve::make_segment);
 	m.def("make_polysegment", servoce::curve::make_polysegment, py::arg("pnts"), py::arg("closed") = false);
 	m.def("make_interpolate", (servoce::wire(*)(const std::vector<servoce::point3>&, const std::vector<servoce::vector3>&, bool))&servoce::curve::make_interpolate, py::arg("pnts"), py::arg("tang"), py::arg("closed") = false);
+	m.def("make_interpolate", (servoce::wire(*)(const std::vector<servoce::point3>&, const bool))&servoce::curve::make_interpolate, py::arg("pnts"), py::arg("closed") = false);
 	m.def("make_helix", servoce::curve::make_helix, py::arg("pitch"), py::arg("height"), py::arg("radius"), py::arg("angle") = 0, py::arg("leftHanded") = false, py::arg("newStyle") = true);
 	m.def("make_long_helix", servoce::curve::make_long_helix, py::arg("pitch"), py::arg("height"), py::arg("radius"), py::arg("angle") = 0, py::arg("leftHanded") = false);
+	m.def("make_complex_wire", servoce::curve::make_complex_wire, py::arg("wires"));
 
 	py::class_<servoce::sweep_solid, servoce::solid>(m, "SolidSweep");
+	py::class_<servoce::sweep_face, servoce::face>(m, "FaceSweep");
 	
 	py::class_<servoce::scene>(m, "Scene")
 		.def(py::init<>())
