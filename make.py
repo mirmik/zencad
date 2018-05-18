a6951d44bfffcca64b9762275be456bf2806c735
mirmik <mirmikns@yandex.ru>
2018-05-18 15:56:30 +0300

    a

--- make.py
+++ make.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from licant.cxx_modules import shared_library
-from licant.modules import module
+from licant.modules import module, submodule
 import licant
+import licant.libs
 
 python_include_prefix = "/usr/include/"
+
+licant.libs.include("servoce")
 
 def registry_library(py):
 	shared_library("zenlib." + py,
@@ -13,7 +16,7 @@ def registry_library(py):
 		
 		cxx_flags = '-fPIC -DQT_NO_VERSION_TAGGING',
 		cc_flags = '-fPIC',
-		ld_flags = '-Wl,-rpath,\'$ORIGIN/\' -L../servoce/build',
+		ld_flags = '-Wl,-rpath,\'$ORIGIN/\'',
 	
 		srcdir = "src",
 		sources = [
@@ -22,11 +25,16 @@ def registry_library(py):
 		include_paths = [
 			python_include_prefix + py
 		],
-		libs = ["servoce"]
+
+		include_modules = [
+			(("servoce_sources"))
+		],
+
+		#libs = ["servoce"]
 	)
 
-licant.make.source("../servoce/build/libservoce.so")
-licant.make.copy(tgt = "zencad/libservoce.so", src = "../servoce/build/libservoce.so")
+#licant.make.source("/usr/lib/libservoce.so")
+#licant.make.copy(tgt = "zencad/libservoce.so", src = "/usr/lib/libservoce.so")
 
 registry_library("python2.7")
 registry_library("python3.5")
@@ -43,29 +51,29 @@ def local35():
 	os.system("rm zencad/libservoce.so")
 	os.system("rm zencad/zenlib.so")
 	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5/zenlib.so")
-	licant.do("zencad/libservoce.so")
+#	licant.do("zencad/libservoce.so")
 	licant.do("zencad/zenlib.so", "makefile")
 
 @licant.routine
 def install35():
-	os.system("rm zencad/libservoce.so")
+#	os.system("rm zencad/libservoce.so")
 	os.system("rm zencad/zenlib.so")
 	os.system("rm zencad/python3.5/zenlib.so")
 	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5/zenlib.so")
-	licant.do("zencad/libservoce.so")
+#	licant.do("zencad/libservoce.so")
 	licant.do("zencad/zenlib.so", "makefile")
 	install_egg("3.5")
 
 @licant.routine
 def local36():
-	os.system("rm zencad/libservoce.so")
+#	os.system("rm zencad/libservoce.so")
 	os.system("rm zencad/zenlib.so")
 	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.6/zenlib.so")
 	licant.do("zencad/zenlib.so", "makefile")
 
 @licant.routine
 def install36():
-	os.system("rm zencad/libservoce.so")
+#	os.system("rm zencad/libservoce.so")
 	os.system("rm zencad/zenlib.so")
 	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5/zenlib.so")
 	licant.do("zencad/zenlib.so", "makefile")
@@ -73,21 +81,21 @@ def install36():
 
 @licant.routine
 def local27():
-	os.system("rm zencad/libservoce.so")
+#	os.system("rm zencad/libservoce.so")
 	os.system("rm zencad/zenlib.so")
 	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python2.7/zenlib.so")
 	licant.do("zencad/zenlib.so", "makefile")
 
 @licant.routine
 def install27():
-	os.system("rm zencad/libservoce.so")
+#	os.system("rm zencad/libservoce.so")
 	os.system("rm zencad/zenlib.so")
 	licant.make.copy(tgt = "zencad/zenlib.so", src = "zencad/python3.5/zenlib.so")
 	licant.do("zencad/zenlib.so", "makefile")
 	install_egg("2.7")
 
 licant.add_makefile_target(tgt = "all", targets = [
-	"zencad/libservoce.so",
+#	"zencad/libservoce.so",
 	"zenlib.python2.7",
 	"zenlib.python3.5",
 	"zenlib.python3.6",
@@ -120,7 +128,7 @@ def distclean():
 	os.system("rm -rf zencad/python*")
 	os.system("rm -rf zencad/__pycache__")
 	os.system("rm -rf zencad/zenlib.so")
-	os.system("rm -rf zencad/libservoce.so")
+#	os.system("rm -rf zencad/libservoce.so")
 	os.system("rm -rf zencad.egg-info")
 	print("distclean success")
 
