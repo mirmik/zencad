89fc067f7639322898968b9bb955485e2d07e9c4
mirmik <mirmikns@yandex.ru>
2018-10-27 13:12:19 +0500

    pagedu zoom support

--- setup.py
+++ setup.py
@@ -4,20 +4,23 @@ from wheel.bdist_wheel import bdist_wheel as bdist_wheel_
 from setuptools import setup, Extension, Command
 from distutils.util import get_platform
 
+import zencad
+
 import sys
 
-try:
-	import PyQt5
-except:
-	print("PyQt5 needed.")
-	print("try:")
-	print("apt-get install python3-pyqt5")
-	sys.exit()
+#try:
+#	import PyQt5
+#except Exception(e):
+#	print(e)
+#	print("PyQt5 needed.")
+#	print("try:")
+#	print("apt-get install python3-pyqt5")
+#	sys.exit()
 
 setup(
 	name = 'zencad',
 	packages = ['zencad'],
-	version = '0.6.3',
+	version = zencad.__version__,
 	license='MIT',
 	description = 'CAD system for righteous zen programmers ',
 	author = 'Sorokin Nikolay',
