cf388b12d0df10b2fcfdc6cc6c5b93f6520f0e82
mirmik <mirmikns@yandex.ru>
2018-10-30 06:45:03 +0300

    a
    Former-commit-id: fe08af2948f192fdfe3ebd32fe3db772f66aa6c3

--- test/caching/torus.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#coding: utf-8
-
-import zencad
-m = zencad.torus(r1 = 10, r2 = 3)
-
-zencad.display(m)
-zencad.show()
--- test/prim1d/segment.py
+++ test/prim1d/segment.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
-import zencad
-m = zencad.cone(r1 = 20, r2 = 10, h = 20, center = True)
+import sys
+sys.path.insert(0, "../..")
 
+import zencad
+
+m = zencad.segment((0,0,0), (10,20,30))
 zencad.display(m)
+
 zencad.show()
