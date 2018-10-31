163bd2bfaa958483bc9c9a625ee2b3e5bd7cd99a
mirmik <mirmikns@yandex.ru>
2018-10-31 14:57:10 +0300

    a
    Former-commit-id: 2cac0079bf6d0a76f434ad1ea2fcc41af89e369c

--- test/screens/screens.py
+++ test/screens/screens.py
@@ -4,10 +4,12 @@
 
 from zencad import *
 
-screen(model=box(10,20,30), path="box.png")
-screen(model=sphere(r=10), path="sphere.png")
-screen(model=cylinder(r=10, h=20), path="cylinder.png")
+wsize = (400,300)
 
-screen(model=cone(r1=20, r2=10, h=20), path="cone.png")
-screen(model=cylinder(r=10, h=20, angle=deg(50)), path="cylinder_sector.png")
-screen(model=torus(r1=20, r2=5), path="torus.png")
+screen(model=box(10,20,30), path="box.png", size=wsize)
+screen(model=sphere(r=10), path="sphere.png", size=wsize)
+screen(model=cylinder(r=10, h=20), path="cylinder.png", size=wsize)
+
+screen(model=cone(r1=20, r2=10, h=20), path="cone.png", size=wsize)
+screen(model=cylinder(r=10, h=20, angle=deg(50)), path="cylinder_sector.png", size=wsize)
+screen(model=torus(r1=20, r2=5), path="torus.png", size=wsize)
