f4c9d3442225fb5c0117477f5949f58869c9c34d
mirmik <mirmikns@yandex.ru>
2018-11-17 12:25:13 +0300

    a

--- docs/images/imagen.py
+++ docs/images/imagen.py
@@ -2,16 +2,25 @@
 #coding: utf-8
 
 from zencad import *
+import os
 
 wsize = (400, 300)
 
-screen(model=box(10,20,30), path="box.png", size=wsize)
-screen(model=sphere(r=10), path="sphere.png", size=wsize)
-screen(model=cylinder(r=10, h=20), path="cylinder.png", size=wsize)
+if not os.path.exists("generic"):
+    os.makedirs("generic")
 
-screen(model=cone(r1=20, r2=10, h=20), path="cone.png", size=wsize)
-screen(model=cylinder(r=10, h=20, angle=deg(50)), path="cylinder_sector.png", size=wsize)
-screen(model=torus(r1=20, r2=5), path="torus.png", size=wsize)
+def doscreen(model, path, size):
+	print("screen (path:{0}, size:{1})".format(path, size))
+	screen(model, path, size)
+	screen(model, "generic/"+path, size)
 
-screen(model=circle(r=20), path="circle.png", size=wsize)
-screen(model=ngon(r=20, n=6), path="ngon.png", size=wsize)
+doscreen(model=box(10,20,30), path="box.png", size=wsize)
+doscreen(model=sphere(r=10), path="sphere.png", size=wsize)
+doscreen(model=cylinder(r=10, h=20), path="cylinder.png", size=wsize)
+
+doscreen(model=cone(r1=20, r2=10, h=20), path="cone.png", size=wsize)
+doscreen(model=cylinder(r=10, h=20, angle=deg(50)), path="cylinder_sector.png", size=wsize)
+
+doscreen(model=torus(r1=20, r2=5), path="torus.png", size=wsize)
+doscreen(model=circle(r=20), path="circle.png", size=wsize)
+doscreen(model=ngon(r=20, n=6), path="ngon.png", size=wsize)
