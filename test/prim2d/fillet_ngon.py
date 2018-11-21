a85fd1be612257324d584f31ad9830ea8b0dd798
mirmik <mirmikns@yandex.ru>
2018-11-22 00:26:56 +0300

    a

--- test/prim2d/fillet_ngon.py
+++ test/prim2d/fillet_ngon.py
@@ -3,7 +3,11 @@
 
 import zencad
 
-m = zencad.ngon(r = 10, n = 6).fillet(1, list([0,1,2,3,4,5]))
-zencad.display(m)
+m1 = zencad.ngon(r = 10, n = 6).fillet(3, (3,4,5))
+m2 = zencad.ngon(r = 10, n = 6).fillet(3, (0,1,2))
+m3 = zencad.ngon(r = 10, n = 6).fillet(3)
+zencad.display(m1)
+zencad.display(m2.left(30))
+zencad.display(m3.right(30))
 
 zencad.show()
