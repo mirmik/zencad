cd7b68d864b5304d1de69e2f9a3f962e13330569
mirmik <mirmikns@yandex.ru>
2018-12-03 14:33:26 +0500

    api

---
+++ utest/prim2d_test.py
@@ -0,0 +1,18 @@
+import unittest
+from zencad import *
+
+class Prim2dProbber(unittest.TestCase):
+	def test_polygon(self):
+		pass
+
+	def test_ngon(self):
+		pass
+
+	def test_rectangle(self):
+		pass
+
+	def test_square(self):
+		pass
+
+	def test_circle(self):
+		pass
--- utest/start.py
+++ utest/start.py
@@ -7,21 +7,16 @@ sys.path.insert(0, "..")
 import unittest
 import zencad
 
+import prim1d_test
+import prim2d_test
 import prim3d_test
+	
+def execute_test(test):
+	print()
+	print("TEST:")
+	unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromModule(test))
 
-class ZenCadApiProbber(unittest.TestCase):
-	def test_api_probber(self):
-		zencad.box(1,2,3).unlazy()
-		zencad.cylinder(r=3,h=3).unlazy()
-		zencad.cylinder(r=3,h=3,angle=20).unlazy()
-		zencad.cone(r1=3,r2=6,h=3).unlazy()
-		#zencad.cone(r1=3,r2=6,h=3,angle=20).unlazy()
-		zencad.torus(r1=3,r2=1).unlazy()
-		#zencad.torus(r1=3,r2=1,angle=20).unlazy()
-		zencad.sphere(r=3).unlazy()
-	
 if __name__ == '__main__':
-	prim3d = unittest.TestLoader().loadTestsFromModule(prim3d_test)
-	unittest.TextTestRunner(verbosity=2).run(prim3d)
-	
-	unittest.main()
+	execute_test(prim1d_test)
+	execute_test(prim2d_test)
+	execute_test(prim3d_test)
