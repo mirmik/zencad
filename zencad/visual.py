fdd30a2da79a5ed693d31bece42500eaa3125790
mirmik <mirmikns@yandex.ru>
2019-01-03 01:07:35 +0500

    a
    Former-commit-id: f46f27dae60ca687553ea055cf59c41a50eb9c78

--- zencad/visual.py
+++ zencad/visual.py
@@ -27,3 +27,23 @@ def screen(model, path, size=(800,600)):
 	image.putdata(pixels)
 
 	image.save(path)
+
+def screen_debug(model, size=(800,600)):
+	scn = Scene()
+	scn.add(model)
+	viewer = Viewer(scn)
+	view = viewer.create_view()
+	view.set_virtual_window(size[0], size[1])
+	view.fit_all()
+
+	raw = view.rawarray()
+	npixels = np.reshape(np.asarray(raw), (size[1],size[0],3))
+	nnnpixels = np.flip(npixels, 0).reshape((size[0] * size[1] * 3))
+
+	rawiter = iter(nnnpixels)
+	pixels = list(zip(rawiter, rawiter, rawiter))
+		
+	image = Image.new("RGB", (size[0], size[1]))
+	image.putdata(pixels)
+
+	image.show()
