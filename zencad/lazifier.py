a8621ccfbaa510ed607489e7fc91cb66e54a8ee2
mirmik <mirmikns@yandex.ru>
2019-01-04 02:07:00 +0500

    a

--- zencad/lazifier.py
+++ zencad/lazifier.py
@@ -9,9 +9,6 @@ cachepath = os.path.expanduser("~/.zencad-cache")
 algo = hashlib.sha512
 
 lazy = evalcache.Lazy(cache = evalcache.DirCache(cachepath), algo = algo)
-lazyhash_fast = evalcache.LazyHash(algo = algo, fastdo = True)
-lazyhash = evalcache.LazyHash(algo = algo, fastdo = False)
-lazyfile = LazyFile()
 
 def disable_cache():
 	lazy.encache = False
