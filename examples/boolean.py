22824eacadb1432876ecfe47d888fc1c03bb98f4
mirmik <mirmikns@yandex.ru>
2019-01-03 21:26:43 +0500

    a
    Former-commit-id: 5bfa3fffeb5151feb4c38f4838372ad9056cff84

--- examples/boolean.py
+++ examples/boolean.py
@@ -21,19 +21,19 @@ m3 = s1 + s2 + s3
 ystep = 200
 xstep = 200
 
-font = "/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf"
+#font = "/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf"
 
-t1 = textshape("difference", font, 40)
-t1c = t1.center()
-t1=t1.translate(-t1c.x, -t1c.y, 0).rotateZ(deg(45))
-
-t2 = textshape("intersect", font, 40)
-t2c = t2.center()
-t2=t2.translate(-t2c.x, -t2c.y, 0).rotateZ(deg(45))
-
-t3 = textshape("union", font, 40)
-t3c = t3.center()
-t3=t3.translate(-t3c.x, -t3c.y, 0).rotateZ(deg(45))
+#t1 = textshape("difference", font, 40)
+#t1c = t1.center()
+#t1=t1.translate(-t1c.x, -t1c.y, 0).rotateZ(deg(45))
+#
+#t2 = textshape("intersect", font, 40)
+#t2c = t2.center()
+#t2=t2.translate(-t2c.x, -t2c.y, 0).rotateZ(deg(45))
+#
+#t3 = textshape("union", font, 40)
+#t3c = t3.center()
+#t3=t3.translate(-t3c.x, -t3c.y, 0).rotateZ(deg(45))
 
 display(base.forw(ystep))
 
@@ -49,8 +49,8 @@ display(m1.back(ystep))
 display(m2.left(xstep).back(ystep))
 display(m3.right(xstep).back(ystep))
 
-display(t1.back(ystep).up(70))
-display(t2.left(xstep).back(ystep).up(70))
-display(t3.right(xstep).back(ystep).up(70))
+#display(t1.back(ystep).up(70))
+#display(t2.left(xstep).back(ystep).up(70))
+#display(t3.right(xstep).back(ystep).up(70))
 
 show()
