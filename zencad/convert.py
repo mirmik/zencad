4b2353f8da57f95a562bf574d642502a36da908e
mirmik <mirmikns@yandex.ru>
2019-01-04 02:02:09 +0500

    a

--- zencad/convert.py
+++ zencad/convert.py
@@ -18,11 +18,11 @@ from zencad.lazifier import lazyfile
 from zencad.lazifier import lazyhash
 from zencad.lazifier import lazy
 
-@lazyfile("path")
+@lazy.lazyfile("path")
 def to_stl(model, path, delta):
 	pyservoce.make_stl(model, path, delta)
 
-@lazyfile("path")
+@lazy.lazyfile("path")
 def to_brep(model, path):
 	pyservoce.brep_write(model, path)
 
