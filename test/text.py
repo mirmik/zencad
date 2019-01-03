7b5e46fa33cd12162dd08fb2dbfe22751eca455b
mirmik <mirmikns@yandex.ru>
2019-01-03 16:18:21 +0500

    a
    Former-commit-id: 893b63a1fe6cad04c5f2553b05aeaff8e70df624

---
+++ test/text.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+from zencad import *
+
+fontpath = "/home/mirmik/project/privdocs/bujin/poster/fonts/keetano_katakana.ttf"
+m = textshape("Mirmik was here", fontpath, 20)
+
+m = m.extrude(3)
+
+display(m)
+show()
