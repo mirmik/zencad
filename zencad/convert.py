f598134944bcba9ccb412115810c09c6d24ea3e8
mirmik <mirmikns@yandex.ru>
2019-01-04 00:38:42 +0500

    a

---
+++ zencad/convert.py
@@ -0,0 +1,26 @@
+import os
+import zencad
+import pyservoce
+import evalcache
+from zencad.lazifier import lazyfile
+from zencad.lazifier import lazyhash
+from zencad.lazifier import lazy
+
+@lazyfile("path")
+def to_stl(model, path, delta):
+	pyservoce.make_stl(model, path, delta)
+
+@lazyfile("path")
+def to_brep(model, path):
+	pyservoce.brep_write(model, path)
+
+def from_brep(path):
+	def impl(path):
+		return pyservoce.brep_read(path)
+	f = lazy(impl, hint = str(os.path.getmtime(path)))
+	obj = f(path)
+	evalcache.encache(obj, False)
+	evalcache.decache(obj, False)
+	return obj
+
+
