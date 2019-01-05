f9e8a3029784b354863d51bc90cdbbac6ffe87eb
mirmik <mirmikns@yandex.ru>
2019-01-05 16:50:30 +0500

    'zencad main'

--- zencad/__main__.py
+++ zencad/__main__.py
@@ -7,10 +7,8 @@ import os
 def banner(): print(zencad.shower.BANNER_TEXT)
 def about(): print(zencad.shower.ABOUT_TEXT)
 
-#banner()
-#about()
-
-print(zencad.moduledir)
+banner()
+about()
 
 logo = zencad.textshape("zencad", os.path.join(zencad.moduledir, "examples/fonts/mandarinc.ttf"), 20)
 zencad.display(logo, zencad.Color(1,1,1,0))
