eac044662482b95f02ff199985f93d95099e6c2b
mirmik <mirmikns@yandex.ru>
2019-01-05 16:51:46 +0500

    'zencad main'
    Former-commit-id: 06f43bbfaf01195c6bd6048822b9ac5d36cd1622

--- zencad/__main__.py
+++ zencad/__main__.py
@@ -4,12 +4,6 @@
 import zencad
 import os
 
-def banner(): print(zencad.shower.BANNER_TEXT)
-def about(): print(zencad.shower.ABOUT_TEXT)
-
-banner()
-about()
-
 logo = zencad.textshape("zencad", os.path.join(zencad.moduledir, "examples/fonts/mandarinc.ttf"), 20)
 zencad.display(logo, zencad.Color(1,1,1,0))
 
