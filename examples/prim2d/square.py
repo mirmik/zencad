adf1dde6416782f826dedee490f3f092fd7bc74d
mirmik <mirmikns@yandex.ru>
2019-01-05 17:59:08 +0500

    'some examples bug fix'

---
+++ examples/prim2d/square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+from zencad import *
+test_mode()
+
+m = square(a = 10, center = True)
+display(m)
+
+show()
--- test/prim2d/rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#coding: utf-8
-
-import zencad
-
-m = zencad.rectangle(a = 10, b = 6, center = True)
-zencad.display(m)
-
-zencad.show()
