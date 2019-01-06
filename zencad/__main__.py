ecb015f155c7ffbddaf488520aeaf33d87eb6e4a
mirmik <mirmikns@yandex.ru>
2019-01-07 03:07:09 +0500

    'simplify helloworld'
    Former-commit-id: 42edc681a17345a5bc0b298113397dd3bcf6da6f

--- zencad/__main__.py
+++ zencad/__main__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 #coding:utf-8
 
-import zencad
-import os
+from zencad import *
 
-logo = zencad.textshape("HelloWorld", os.path.join(zencad.moduledir, "examples/fonts/mandarinc.ttf"), 20)
-zencad.display(logo, zencad.Color(1,1,1,0))
+logo = textshape("HelloWorld", 
+	moduledir + "/examples/fonts/mandarinc.ttf", 20)
+display(logo, Color(1,1,1,0))
 
-zencad.show(showconsole=True, showeditor=True)
+show(showconsole=True, showeditor=True)
