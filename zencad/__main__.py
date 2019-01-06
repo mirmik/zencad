8f68c6bc61c276576f90b45b0f92fa5190f507ad
mirmik <mirmikns@yandex.ru>
2019-01-07 03:04:33 +0500

    'helloworld'
    Former-commit-id: cc60c10ab7b16c6cc5f4e4d3d406f0e517a30c6f

--- zencad/__main__.py
+++ zencad/__main__.py
@@ -4,7 +4,7 @@
 import zencad
 import os
 
-logo = zencad.textshape("zencad", os.path.join(zencad.moduledir, "examples/fonts/mandarinc.ttf"), 20)
+logo = zencad.textshape("HelloWorld", os.path.join(zencad.moduledir, "examples/fonts/mandarinc.ttf"), 20)
 zencad.display(logo, zencad.Color(1,1,1,0))
 
 zencad.show(showconsole=True, showeditor=True)
