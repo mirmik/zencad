d1b70ce16baa772adfb235d82dc412f7c5da633c
mirmik <mirmikns@yandex.ru>
2019-01-07 20:36:21 +0500

    'camera animate'
    Former-commit-id: d61aa1023faecd7f785388df35a6c2a0a1629841

--- zencad/examples/prim3d/torus.py
+++ zencad/examples/prim3d/torus.py
@@ -15,9 +15,9 @@ m1 = torus(r1=tor_r1, r2=tor_r2, uangle=tor_u)
 m2 = torus(r1=tor_r1, r2=tor_r2, vangle=(tor_v1,tor_v2))
 m3 = torus(r1=tor_r1, r2=tor_r2, vangle=(tor_v1,tor_v2), uangle=tor_u)
 
-display(m0.right(30*0))
-display(m1.right(30*1))
-display(m2.right(30*2))
-display(m3.right(30*3))
+display(m0.right(20*0))
+display(m1.right(20*1))
+display(m2.right(20*2))
+display(m3.right(20*3))
 
 show()
