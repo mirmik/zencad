cce1ac07c057ec036b94bfe1c01ea8076d5013a9
mirmik <mirmikns@yandex.ru>
2019-01-13 21:43:53 +0300

    'a'
    Former-commit-id: 1d628f92f0fb2be9bb8145935627c29f04924359

---
+++ zencad/surface.py
@@ -0,0 +1,8 @@
+import zencad
+import pyservoce
+
+from zencad.lazifier import lazy, nocached_shape_generator, shape_generator
+
+@lazy.lazy(cls=nocached_shape_generator)
+def cylinder(r):
+	return pyservoce.surface_cylinder(r)
