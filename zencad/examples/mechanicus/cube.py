9b5bb2748f30d9108416282a17dfa0f998a02d55
mirmik <mirmikns@yandex.ru>
2019-01-15 11:35:16 +0300

    'mechanicus and animeate'

--- zencad/examples/mechanicus/cube.py
+++ zencad/examples/mechanicus/cube.py
@@ -18,30 +18,30 @@ mechanicus, base = mech.buildpair()
 
 cube = box(1000, center=True)
 
-#mech = [display(mechanicus, zencad.Color(1,1,1)) for i in range(0,6)]
-#base = [display(base, zencad.Color(0.2,0.2,0.2)) for i in range(0,6)]
-#
-#i = 0
-#def animate(wdg):
-#	global i
-#	i += 0.01
-#	trans = rotateZ(i) * translate(0,0,500)
-#	mech[0].set_location(trans)
-#	base[0].set_location(trans)
-#	mech[1].set_location(rotateX(deg(180)) * trans)
-#	base[1].set_location(rotateX(deg(180)) * trans)
-#	mech[2].set_location(rotateX(deg(90)) * trans)
-#	base[2].set_location(rotateX(deg(90)) * trans)
-#	mech[3].set_location(rotateX(deg(90)) * rotateX(deg(180)) * trans)
-#	base[3].set_location(rotateX(deg(90)) * rotateX(deg(180)) * trans)
-#	mech[4].set_location(rotateY(deg(90)) * trans)
-#	base[4].set_location(rotateY(deg(90)) * trans)
-#	mech[5].set_location(rotateY(deg(90)) * rotateX(deg(180)) * trans)
-#	base[5].set_location(rotateY(deg(90)) * rotateX(deg(180)) * trans)
-#	wdg.view.redraw()
-#
-#display(cube, Color(0.3, 0.2, 0.2))
-##show(animate = animate)
+mech = [display(mechanicus, zencad.Color(1,1,1)) for i in range(0,6)]
+base = [display(base, zencad.Color(0.2,0.2,0.2)) for i in range(0,6)]
+
+i = 0
+def animate(wdg):
+	global i
+	i += 0.01
+	trans = rotateZ(i) * translate(0,0,500)
+	mech[0].set_location(trans)
+	base[0].set_location(trans)
+	mech[1].set_location(rotateX(deg(180)) * trans)
+	base[1].set_location(rotateX(deg(180)) * trans)
+	mech[2].set_location(rotateX(deg(90)) * trans)
+	base[2].set_location(rotateX(deg(90)) * trans)
+	mech[3].set_location(rotateX(deg(90)) * rotateX(deg(180)) * trans)
+	base[3].set_location(rotateX(deg(90)) * rotateX(deg(180)) * trans)
+	mech[4].set_location(rotateY(deg(90)) * trans)
+	base[4].set_location(rotateY(deg(90)) * trans)
+	mech[5].set_location(rotateY(deg(90)) * rotateX(deg(180)) * trans)
+	base[5].set_location(rotateY(deg(90)) * rotateX(deg(180)) * trans)
+	wdg.view.redraw()
+
+display(cube, Color(0.3, 0.2, 0.2))
+show(animate = animate)
 
 #scn = Scene()
 #mech = [scn.add(mechanicus.unlazy(), zencad.Color(1,1,1)) for i in range(0,6)]
