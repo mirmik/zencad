40c5e053d07ad798008117f29315d41390e5777b
mirmik <mirmikns@yandex.ru>
2019-01-31 17:52:25 +0300

    'scale operation is caching now'
    Former-commit-id: 57c777a4e6f67113eb28edbbb11b29650bb85a76

--- zencad/transform.py
+++ zencad/transform.py
@@ -9,76 +9,87 @@ import sys
 import operator
 import numpy as np
 
-class LazyObjectTransformGenerator(evalcache.LazyObject):
+class LazyObjectTransformGeneratorCached(evalcache.LazyObject):
 	def __init__(self, *args, **kwargs): evalcache.LazyObject.__init__(self, *args, **kwargs)
-	def __call__(self, *args, **kwargs): return evalcache.lazy.lazyinvoke(self, self, args, kwargs, encache=False, decache=False, cls=LazyObjectTransform)
+	def __call__(self, *args, **kwargs): return evalcache.lazy.lazyinvoke(self, self, args, kwargs, encache=False, decache=False, cls=LazyObjectTransformCached)
 
-class LazyObjectTransform(evalcache.LazyObject):
+class LazyObjectTransformGeneratorNoCached(evalcache.LazyObject):
 	def __init__(self, *args, **kwargs): evalcache.LazyObject.__init__(self, *args, **kwargs)
-	def __mul__(self, oth): return evalcache.lazy.lazyinvoke(self, operator.__mul__, (self, oth), encache=False, decache=False, cls=LazyObjectTransform)
+	def __call__(self, *args, **kwargs): return evalcache.lazy.lazyinvoke(self, self, args, kwargs, encache=False, decache=False, cls=LazyObjectTransformNoCached)
+
+class LazyObjectTransformCached(evalcache.LazyObject):
+	def __init__(self, *args, **kwargs): evalcache.LazyObject.__init__(self, *args, **kwargs)
+	def __mul__(self, oth): return evalcache.lazy.lazyinvoke(self, operator.__mul__, (self, oth), encache=False, decache=False, cls=LazyObjectTransformCached)
+	def __call__(self, *args, **kwargs): return evalcache.lazy.lazyinvoke(self, self, args, kwargs, cls=LazyObjectShape)
+	
+class LazyObjectTransformNoCached(evalcache.LazyObject):
+	def __init__(self, *args, **kwargs): evalcache.LazyObject.__init__(self, *args, **kwargs)
+	def __mul__(self, oth): return evalcache.lazy.lazyinvoke(self, operator.__mul__, (self, oth), encache=False, decache=False, cls=LazyObjectTransformCached)
 	def __call__(self, *args, **kwargs): return evalcache.lazy.lazyinvoke(self, self, args, kwargs, encache=False, decache=False, cls=LazyObjectShape)
 	
-evalcache.lazy.hashfuncs[LazyObjectTransform] = evalcache.lazy.updatehash_LazyObject
-evalcache.lazy.hashfuncs[LazyObjectTransformGenerator] = evalcache.lazy.updatehash_LazyObject
+evalcache.lazy.hashfuncs[LazyObjectTransformNoCached] = evalcache.lazy.updatehash_LazyObject
+evalcache.lazy.hashfuncs[LazyObjectTransformCached] = evalcache.lazy.updatehash_LazyObject
+evalcache.lazy.hashfuncs[LazyObjectTransformGeneratorCached] = evalcache.lazy.updatehash_LazyObject
+evalcache.lazy.hashfuncs[LazyObjectTransformGeneratorNoCached] = evalcache.lazy.updatehash_LazyObject
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def translate(*args, **kwargs): return pyservoce.translate(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def up(*args, **kwargs): return pyservoce.up(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def down(*args, **kwargs): return pyservoce.down(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def left(*args, **kwargs): return pyservoce.left(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def right(*args, **kwargs): return pyservoce.right(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def forw(*args, **kwargs): return pyservoce.forw(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def back(*args, **kwargs): return pyservoce.back(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def rotateX(*args, **kwargs): return pyservoce.rotateX(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def rotateY(*args, **kwargs): return pyservoce.rotateY(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def rotateZ(*args, **kwargs): return pyservoce.rotateZ(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def mirrorXZ(*args, **kwargs): return pyservoce.mirrorXZ(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def mirrorYZ(*args, **kwargs): return pyservoce.mirrorYZ(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def mirrorXY(*args, **kwargs): return pyservoce.mirrorXY(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def mirrorX(*args, **kwargs): return pyservoce.mirrorX(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def mirrorY(*args, **kwargs): return pyservoce.mirrorY(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorNoCached)
 def mirrorZ(*args, **kwargs): return pyservoce.mirrorZ(*args, **kwargs)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorCached)
 def scale(factor, center): return pyservoce.scale(factor, point3(center).to_servoce())
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorCached)
 def scaleX(factor): return pyservoce.scaleX(factor)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorCached)
 def scaleY(factor): return pyservoce.scaleY(factor)
 
-@lazy.lazy(cls=LazyObjectTransformGenerator)
+@lazy.lazy(cls=LazyObjectTransformGeneratorCached)
 def scaleZ(factor): return pyservoce.scaleZ(factor)
 
 class multitransform:
