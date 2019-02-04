4ec6e8467ea29c0e2f8923b4f77ac7eddf8ef83f
mirmik <mirmikns@yandex.ru>
2019-02-05 01:57:30 +0300

    'a'
    Former-commit-id: 193fb9e5fa758f65ae5a994e55028bce270a84f0

--- zencad/examples/cup.py
+++ zencad/examples/cup.py
@@ -27,19 +27,23 @@ tang = vectors([
 	(0,0),
 ]) 
 
+#Base:
 base = cylinder(r = radius, h = height)
 hole = cylinder(r = radius - thikness, h = height - thikness).up(thikness)
 
+#Handle:
 spine = interpolate(pnts, tang).rotateX(deg(90))
 profile = circle(handle_radius).rotateY(deg(45)).translate(pnts[0].x, 0, pnts[0].y)
 handle = pipe(path = spine, prof = profile)
 
+#Assemble:
+cup = (base + handle.right(40).up(17) - hole)
+
+#Display:
 hl(spine.right(100).up(17).forw(20))
 hl(profile.right(100).up(17).forw(20))
 hl(handle.right(100).up(17).back(20))
-
-cup = (base + handle.right(40).up(17) - hole)
-
 display(cup)
+
 show()
 
