bcd6a0cdd0858122079634c61ad445f000a1606c
mirmik <mirmikns@yandex.ru>
2019-02-06 05:44:01 +0300

    'examples'

--- zencad/examples/prim2d/fillet_ngon.py
+++ zencad/examples/prim2d/fillet_ngon.py
@@ -9,17 +9,26 @@ lazy.diag = True
 ng = ngon(r = 10, n = 6)
 
 #Fillter all vertices
+print("m1:")
 m1 = ngon(r = 10, n = 6).fillet(3)
+m1.unlazy()
 
 #Generator can be used for array filtering
+print("m2:")
 m2 = ng.fillet(3, [v for v in ng.vertices() if v.x < 0])
+m2.unlazy()
 
 #We can use lazy lambda for improve caching algorithm
+print("m3:")
 m3 = ng.fillet(3, lazy(lambda: [v for v in ng.vertices() if v.y < 0])())
+m3.unlazy()
 
-#Alternate syntax (and inaccuracy of float when comparing)
+#Each one syntax variant (and inaccuracy of float when comparing)
+print("m4:")
 m4 = ng.fillet(3, evalcache.select(ng.vertices(), lambda v: abs(v.y) < 0.001))
+m4.unlazy()
 
+print("display:")
 display(m1)
 display(m2.right(30))
 display(m3.right(60))
