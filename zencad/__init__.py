c768365801cbbd291eaacdf32189f0036ed93307
mirmik <mirmikns@yandex.ru>
2019-02-07 01:33:06 +0300

    'examples and refactor'

--- zencad/__init__.py
+++ zencad/__init__.py
@@ -30,29 +30,10 @@ from zencad.prim3d import *
 from zencad.prim2d import *
 from zencad.prim1d import *
 
-@lazy.lazy(cls=shape_generator)
-def linear_extrude(*args, **kwargs):
-	return pyservoce.make_linear_extrude(*args, **kwargs)
+from zencad.ops3d import *
+from zencad.ops1d2d import *
 
-@lazy.lazy(cls=shape_generator)
-def pipe(prof, path):
-	return pyservoce.make_pipe(prof, path)
 
-@lazy.lazy(cls=shape_generator)
-def pipe_shell(prof, path, frenet = False):
-	return pyservoce.make_pipe_shell(prof, path, frenet)
-
-@lazy.lazy(cls=shape_generator)
-def loft(arr):
-	return pyservoce.loft(arr)
-
-@lazy.lazy(cls=shape_generator)
-def revol(shp, angle=0.0):
-	return pyservoce.revol(shp, angle)
-
-@lazy.lazy(cls=shape_generator)
-def thicksolid(shp, pnts, t):
-	return pyservoce.thicksolid(shp, points(pnts), t)
 
 
 @lazy.lazy(cls=shape_generator)
@@ -72,10 +53,10 @@ def interpolate(pnts, tangs=[], closed=False):
 def sew(*args, **kwargs):
 	return pyservoce.sew(*args, **kwargs)
 
-@lazy.lazy(cls=shape_generator)
-def sweep(prof, path):
-	print("def sweep(prof, path): deprecated")
-	return pyservoce.make_sweep(prof, path)
+#@lazy.lazy(cls=shape_generator)
+#def sweep(prof, path):
+#	print("def sweep(prof, path): deprecated")
+#	return pyservoce.make_sweep(prof, path)
 
 def gr(grad): 
 	print("'gr' function is deprecated. Use 'deg' instead")
@@ -97,19 +78,4 @@ def near_face(*args, **kwargs): return pyservoce.near_face(*args, **kwargs)
 @lazy.lazy(cls=shape_generator)
 def near_vertex(*args, **kwargs): return pyservoce.near_vertex(*args, **kwargs)
 
-@lazy.lazy(cls=shape_generator)
-def fillet(shp, r, refs=None): 
-	if refs is None:
-		return pyservoce.fillet(shp, r)
-	else:
-		return pyservoce.fillet(shp, r, points(refs))
 
-@lazy.lazy(cls=shape_generator)
-def chamfer(shp, r, refs=None): 
-	if refs is None:
-		return pyservoce.chamfer(shp, r)
-	else:
-		return pyservoce.chamfer(shp, r, points(refs))
-
-
-pyservoce.Shape.fillet = fillet
---
+++ zencad/ops1d2d.py
@@ -0,0 +1,17 @@
+import pyservoce
+from zencad.lazifier import lazy, shape_generator, nocached_shape_generator
+
+
+@lazy.lazy(cls=shape_generator)
+def fill(*args, **kwargs):
+	return pyservoce.fill(*args, **kwargs)
+
+
+@lazy.lazy(cls=shape_generator)
+def interpolate(pnts, tangs=[], closed=False):
+	return pyservoce.interpolate(points(pnts), vectors(tangs), closed)
+
+
+@lazy.lazy(cls=shape_generator)
+def sew(*args, **kwargs):
+	return pyservoce.sew(*args, **kwargs)
