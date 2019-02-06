8bff0fdb83161c5da06a11fa4779b19743a7c644
mirmik <mirmikns@yandex.ru>
2019-02-06 07:02:02 +0300

    'examples'
    Former-commit-id: 0a99340740638fac055a9bafcc9941a494b308e7

--- zencad/examples/prim2d/textshape.py
+++ zencad/examples/prim2d/textshape.py
@@ -4,7 +4,7 @@
 import os
 from zencad import *
 
-fontpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fonts/testfont.ttf")
+fontpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../fonts/testfont.ttf")
 
 m = textshape(text="TextShape", fontpath=fontpath, size=100)
 
