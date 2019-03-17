64ac8e666728799aef66203e8149b586b3a1873d
mirmik <mirmikns@yandex.ru>
2019-03-17 23:18:09 +0300

    'Add point, vector math operations and transform invert method'

---
+++ utest/math_test.py
@@ -0,0 +1,16 @@
+import unittest
+from zencad import *
+
+def early(a, b):
+	if abs(a.x - b.x) > 0.0001: return False
+	if abs(a.y - b.y) > 0.0001: return False
+	if abs(a.z - b.z) > 0.0001: return False
+	return True
+
+class MathTest(unittest.TestCase):
+	def test_math(self):
+		self.assertTrue(early(zencad.point3( 1,2,3) + zencad.vector3(7,6,5), zencad.point3(  8, 8, 8)))
+		self.assertTrue(early(zencad.point3( 1,2,3) - zencad.vector3(7,6,5), zencad.point3( -6,-4,-2)))
+		self.assertTrue(early(zencad.vector3(1,2,3) + zencad.vector3(7,6,5), zencad.vector3( 8, 8, 8)))
+		self.assertTrue(early(zencad.vector3(1,2,3) - zencad.vector3(7,6,5), zencad.vector3(-6,-4,-2)))
+		self.assertTrue(early(zencad.point(  1,2,3) - zencad.point3( 7,6,5), zencad.vector3(-6,-4,-2)))
