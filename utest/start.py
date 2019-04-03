c780b61cbd093ae12376e0f17a98b0a11bc79e18
mirmik <mirmikns@yandex.ru>
2019-04-03 20:15:25 +0300

    'boolean_test'
    Former-commit-id: dabb0445e6b96ae86ed9cc9e39509e311aaa5dd7

--- utest/start.py
+++ utest/start.py
@@ -10,6 +10,7 @@ import zencad
 import prim1d_test
 import prim2d_test
 import prim3d_test
+import boolean_test
 import math_test
 	
 def execute_test(test):
@@ -21,4 +22,5 @@ if __name__ == '__main__':
 	execute_test(prim1d_test)
 	execute_test(prim2d_test)
 	execute_test(prim3d_test)
+	execute_test(boolean_test)
 	execute_test(math_test)
