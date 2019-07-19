7b7bc8f306a025ef21facf9547b59fe9aebe70bf
mirmik <mirmikns@yandex.ru>
2019-07-19 14:59:00 +0300

    'cynematic'
    Former-commit-id: a21c4ed4786a905fc491a7bb03177fe27f2ec667

--- zencad/cynematic.py
+++ zencad/cynematic.py
@@ -1,5 +1,106 @@
-import zencad.controllers
+import zencad.assemble
 import pyservoce
+
+from abc import ABC, abstractmethod
+
+class cynematic_unit(ABC, zencad.assemble.unit):
+	"""Кинематическое звено задаётся двумя системами координат,
+	входной и выходной. Изменение кинематических параметров изменяет
+	положение выходной СК относительно входной"""
+
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.output = zencad.assemble.unit(parent=self)
+
+	@abstractmethod
+	def senses(self):
+		"""Возвращает кортеж тензоров производной по положению
+		по набору кинематических координат
+		в собственной системе координат в формате (w, v)"""
+
+		raise NotImplementedError
+
+	@abstractmethod
+	def set_coords(self, coords, **kwargs):
+		"""Устанавливает модельное положение звена согласно 
+		переданным координатам"""
+		
+		raise NotImplementedError
+
+	def link(self, arg):
+		"""Присоединить объект arg к выходной СК.
+
+		Для cynematic_unit метод link переопределяется,
+		с тем, чтобы линковка происходила не ко входной, 
+		а к выходной СК"""
+
+		self.output.link(arg)
+
+class cynematic_unit_one_axis(cynematic_unit):
+	"""Кинематическое звено специального вида,
+	взаимное положение СК которого может быть описано одним 3вектором
+
+	ax - вектор, задающий ось и направление. задаётся с точностью до длины.
+	mul - линейный коэффициент масштабирования входной координаты.
+	"""
+	
+	def __init__(self, ax, mul=1, **kwargs):
+		super().__init__(**kwargs)
+		self.coord = 0
+		self.ax = pyservoce.vector3(ax)
+		self.ax = self.ax.normalize()
+		self.mul = mul
+		self.axmul = self.ax * self.mul
+
+	#override
+	def senses(self):
+		return (sensivity(),)
+
+	#override
+	def set_coords(self, coords, **kwargs):
+		self.set_coord(coords[0], **kwargs)
+
+class rotator(cynematic_unit_one_axis):
+	def sensivity(self):
+		"""Возвращает тензор производной по положению
+		в собственной системе координат в формате (w, v)"""
+		return (pyservoce.vector3(), axmul)
+
+	def set_coord(self, coord):
+		self.coord = coord
+		self.output.relocate(pyservoce.rotate(self.ax, coord * self.mul), **kwargs)
+
+class actuator(cynematic_unit_one_axis):
+	def sensivity(self):
+		"""Возвращает тензор производной по положению
+		в собственной системе координат в формате (w, v)"""
+		return (axmul, pyservoce.vector3())
+
+	def set_coord(self, coord):
+		self.coord = coord
+		self.output.relocate(pyservoce.translate(self.ax, coord * self.mul), **kwargs)
+
+class planemover(cynematic_unit):
+	"""Кинематическое звено с двумя степенями свободы для перемещения
+	по плоскости"""
+
+	def __init__(self):
+		super().__init__(**kwargs)
+		self.x = 0
+		self.y = 0
+
+	def senses(self):
+		return (
+			(pyservoce.vector3(1,0,0), pyservoce.vector3()),
+			(pyservoce.vector3(0,1,0), pyservoce.vector3())
+		)
+
+	def set_coords(self, coords, **kwargs):
+		self.x = coord[0]
+		self.y = coord[1]
+		self.output.relocate(
+			pyservoce.translate(pyservoce.vector3(self.x, self.y, 0)), **kwargs)
+
 
 class cynematic_chain:
 	def __init__(self, finallink, startlink = None):
@@ -10,7 +111,7 @@ class cynematic_chain:
 	def collect_parametered(self):
 		ret = []
 		for l in self.chain:
-			if isinstance(l, zencad.controllers.CynematicUnit):
+			if isinstance(l, cynematic_unit):
 				ret.append(l)
 		return ret
 
@@ -37,7 +138,7 @@ class cynematic_chain:
 		tmp = None
 
 		for l in chain:
-			if isinstance(l, zencad.controllers.CynematicUnit):
+			if isinstance(l, cynematic_unit):
 				if tmp is not None:
 					ret.append(tmp)
 				ret.append(l)
