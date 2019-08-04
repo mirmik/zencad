70ae211da072aa5b8440eb1620a4f42b9d3ec93a
mirmik <mirmikns@yandex.ru>
2019-08-04 22:16:52 +0300

    'a'
    Former-commit-id: 3dd8352f3f52f3a21af8a8965d7a3929b27d2c68

---
+++ zencad/unbound/communicator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import io
+import base64
+import pickle
+import threading
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class Communicator(QObject):
+	oposite_clossed = pyqtSignal()
+
+	class Listener(QThread):
+		newdata = pyqtSignal(bytes)
+		def __init__(self, ipipe):
+			super().__init__()
+			self.ipipe = ipipe
+			self.file = io.BytesIO()
+
+		def run(self):
+			readFile = os.fdopen(self.ipipe)
+			while(True):
+				inputdata = readFile.readline()
+				print("inputdata:", inputdata)
+
+				if len(inputdata) == 0:
+					self.oposite_clossed.emit()
+					return
+
+				ddd = base64.decodestring(bytes(inputdata, "utf-8"))
+				self.newdata.emit(ddd)
+
+	def __init__(self, ipipe, opipe):
+		super().__init__()
+		self.ipipe = ipipe
+		self.opipe = opipe
+		self.listener_thr = self.Listener(ipipe)
+		self.newdata = self.listener_thr.newdata
+
+	def start_listen(self):
+		self.listener_thr.start()
+
+	def send(self, obj):
+		sendstr = base64.b64encode(pickle.dumps(obj)) + bytes("\n", 'utf-8')
+		os.write(self.opipe, sendstr)
+		#os.flush(self.opipe)
