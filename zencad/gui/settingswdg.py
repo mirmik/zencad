091faba27bf5eef3ed8acce3d64cb8e9b9b5c61f
mirmik <mirmikns@yandex.ru>
2019-09-24 12:36:03 +0300

    'settings start'

---
+++ zencad/gui/settingswdg.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class SettingsWidget(QDialog):
+	"""Виджет настроек системы"""
+
+	def __init__(self):
+		super().__init__(self)
+
