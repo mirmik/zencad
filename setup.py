a8edd89b476dc33b3596ef6a87e5dc32d880ce0f
mirmik <mirmikns@yandex.ru>
2019-09-26 12:30:04 +0300

    'fix open dialog'
    Former-commit-id: 50411ecf94c26053a07063b92e695fdcb699b1bc

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 setup(
     name="zencad",
     packages=["zencad"],
-    version="0.17.1",
+    version="0.17.2",
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
