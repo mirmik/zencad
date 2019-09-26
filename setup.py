05758df86e84079a2dbdee91761bb6b92d1d96ad
mirmik <mirmikns@yandex.ru>
2019-09-26 12:19:39 +0300

    'pyservoce version'
    Former-commit-id: 93aaeb3ae4d9b852dd1807aff00e0f5f57649336

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 setup(
     name="zencad",
     packages=["zencad"],
-    version="0.16.2",
+    version="0.17.1",
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
@@ -36,7 +36,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "evalcache==1.10.1",
-        "pyservoce==1.10.0",
+        "pyservoce>=1.10.0",
         "numpy",
         "pillow",
         "PyQt5",
