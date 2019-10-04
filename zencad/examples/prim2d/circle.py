61b889ce70c7c33899094bfcc5c457dd6c6b2fdb
mirmik <mirmikns@yandex.ru>
2019-10-04 18:19:54 +0300

    'update prim2d examples'
    Former-commit-id: d4156804b087b159e0898e08fcfa9867f34b212d

--- zencad/examples/prim2d/circle.py
+++ zencad/examples/prim2d/circle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-# coding: utf-8
+"""
+ZenCad API example: circle
+last update: 04.10.2019
+"""
 
 from zencad import *
 
@@ -11,11 +14,19 @@ m3 = circle(r=10, wire=True)
 m4 = circle(r=10, angle=deg(90), wire=True)
 m5 = circle(r=10, angle=(deg(45), deg(90)), wire=True)
 
-display(m0.right(30 * 0))
-display(m1.right(30 * 1))
-display(m2.right(30 * 2))
-display(m3.right(30 * 3))
-display(m4.right(30 * 4))
-display(m5.right(30 * 5))
+pacman = ( 
+	circle(r=10, angle=(deg(-135), deg(135))) - 
+	circle(r=1).forw(5)
+)
+
+disp(m0.right(30), color.red)
+disp(m1.right(60), color.green)
+disp(m2.right(90), color.white)
+disp(m3.right(30).back(30), color.red)
+disp(m4.right(60).back(30), color.green)
+disp(m5.right(90).back(30), color.white)
+
+disp(pacman.forw(30).right(30), color.yellow)
+for w in pacman.wires(): disp(w.forw(30).right(60))
 
 show()
