efe79340af6ffcac79581946c5ca25e35446d046
mirmik <mirmikns@yandex.ru>
2019-10-09 18:49:31 +0300

    'init'
    Former-commit-id: b55aec51b4ddb278eba57186d77e99b72189979b

--- zencad/libs/screw.py
+++ zencad/libs/screw.py
@@ -3,8 +3,33 @@
 import zencad
 
 class screw:
-	def __init__(self, ang, lin):
+	"""Геометрический винт. 
+
+	Состоит из угловой и линейной части."""
+
+	def __init__(self, ang=(0,0,0), lin=(0,0,0)):
 		self.ang = zencad.to_vector(ang)
 		self.lin = zencad.to_vector(lin)
 
-	
+	def carry(self, trans):
+		"""Перенос бивектора в другую точку приложения.
+
+		Detail
+		------
+		Формула TODO
+		"""
+		
+		print("TODO carry")
+		return screw(self.ang, self.lin)
+
+	@classmethod
+	def fromtrans(trans):
+		"""Создать винт на основе объекта трансформации zencad.transform
+		
+		Detail:
+		-------
+		Масштабирование игнорируется.
+		"""
+
+		print("TODO fromtrans")
+		return screw()
