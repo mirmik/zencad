1c93c1818f0bfb52e02557ebc5a2fb2ca2b9490f
mirmik <mirmikns@yandex.ru>
2019-10-21 14:52:07 +0300

    'settings first iteration'
    Former-commit-id: 6367712eb9687f1115af0616f5f84f03410ce12e

--- zencad/gui/settings.py
+++ zencad/gui/settings.py
@@ -1,10 +1,48 @@
+#!/usr/bin/env python3
+
 from PyQt5.QtCore import *
 
-list_of_settings = {
-	"text_editor" : "subl",
-	"recent" : []	
-}
+class Settings():
+	list_of_settings = {
+			"gui" : {
+			"text_editor" : "subl"
+		}
+	}
+	
+	#def __init__(self):
+	#	super().__init__()
 
-class Settings(QSettings):
-	def __init__(self):
+	@classmethod
+	def store(self):
+		settings = QSettings("ZenCad", "settings")
+
+		for g in self.list_of_settings:
+			settings.beginGroup(g)
+			for k in self.list_of_settings[g]:
+				settings.setValue(k, self.list_of_settings[g][k])
+			settings.endGroup()
+
+	@classmethod
+	def restore(self):
+		settings = QSettings("ZenCad", "settings")
+
+		for g in self.list_of_settings:
+			settings.beginGroup(g)
+			for k in self.list_of_settings[g]:
+				self.list_of_settings[g][k] = settings.value(k, self.list_of_settings[g][k])
+			settings.endGroup()
+
+	@classmethod
+	def set_editor(self, editor):
+		self.list_of_settings["gui"]["text_editor"] = editor
+		self.store()
+
+
+if __name__ == "__main__":
+	settings = Settings()
+	settings.restore()
+
+	print(settings.list_of_settings)
+
+
 
