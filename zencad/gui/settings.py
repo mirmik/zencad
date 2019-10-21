3a6d4d16db25a51053282e6befe8ed4b4b4cb9cf
mirmik <mirmikns@yandex.ru>
2019-10-21 14:57:11 +0300

    'settings first iteration'
    Former-commit-id: 04ec6acdb4987d406ef3c030f5d52ef1ce5b152f

--- zencad/gui/settings.py
+++ zencad/gui/settings.py
@@ -4,8 +4,13 @@ from PyQt5.QtCore import *
 
 class Settings():
 	list_of_settings = {
-			"gui" : {
-			"text_editor" : "subl"
+		"gui" : {
+			"text_editor" : "subl",
+		},
+		"view" : {
+			"default_color_red" : 0.6,
+			"default_color_green" : 0.6,
+			"default_color_blue" : 0.6,
 		}
 	}
 	
@@ -36,6 +41,20 @@ class Settings():
 	def set_editor(self, editor):
 		self.list_of_settings["gui"]["text_editor"] = editor
 		self.store()
+
+	@classmethod
+	def set_default_color(self, r, g, b):
+		self.list_of_settings["view"]["default_color_red"] = r
+		self.list_of_settings["view"]["default_color_red"] = g
+		self.list_of_settings["view"]["default_color_red"] = b
+
+	@classmethod
+	def get_default_color(self, r, g, b):
+		return (
+			self.list_of_settings["view"]["default_color_red"],
+			self.list_of_settings["view"]["default_color_red"],
+			self.list_of_settings["view"]["default_color_red"]
+		)
 
 
 if __name__ == "__main__":
