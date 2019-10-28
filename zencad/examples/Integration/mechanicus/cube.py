4273973f416793025422ca4c245b1cdf65fdb66e
mirmik <mirmikns@yandex.ru>
2019-10-28 22:29:21 +0300

    'Remove matplotlib from mech example'

--- zencad/examples/Integration/mechanicus/cube.py
+++ zencad/examples/Integration/mechanicus/cube.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import sys
 from skimage import measure, io
 from itertools import chain
