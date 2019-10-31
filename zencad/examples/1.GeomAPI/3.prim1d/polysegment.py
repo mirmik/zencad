b9b64ba79dfe64a531fcb6765ed93c0a2bcbfb8f
mirmik <mirmikns@yandex.ru>
2019-10-31 13:10:55 +0300

    'update examples'
    Former-commit-id: 00a35e1b4cb4989370b87d1f9bc9e360976a0411

--- zencad/examples/1.GeomAPI/3.prim1d/polysegment.py
+++ zencad/examples/1.GeomAPI/3.prim1d/polysegment.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-# coding: utf-8
+
+"""
+ZenCad API example: polysegment.py
+"""
 
 from zencad import *
-
-test_mode()
 
 pnts = points([(0, 0, 0), (0, 10, 10), (0, 10, 20), (0, -10, 20), (0, -10, 10)])
 
@@ -13,10 +14,10 @@ m2 = polysegment(pnts, closed=True).fill()
 m3 = polysegment(pnts + [(0, 0, 0)])
 m4 = polysegment(pnts + [(0, 0, 0)]).fill()
 
-display(m0)
-display(m1.left(20))
-display(m2.left(40))
-display(m3.left(20).forw(30))
-display(m4.left(40).forw(30))
+disp(m0)
+disp(m1.left(20))
+disp(m2.left(40))
+disp(m3.left(20).forw(30))
+disp(m4.left(40).forw(30))
 
 show()
