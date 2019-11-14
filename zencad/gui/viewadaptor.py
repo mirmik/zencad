173fc012d2c70dc1610b864339b1856f27e904f3
mirmik <mirmikns@yandex.ru>
2019-11-14 16:55:43 +0300

    'fix triedron'
    Former-commit-id: 4b28ba751b736a5ed1954e7701c477d081e9e182

--- zencad/gui/viewadaptor.py
+++ zencad/gui/viewadaptor.py
@@ -58,7 +58,7 @@ class DisplayWidget(QGLWidget):
 		self.showmarkers = showmarkers
 
 		self.scene = scene
-		self.scene.viewer.set_triedron(True)
+		self.scene.viewer.set_triedron_axes(True)
 		self.view = view
  
 		self.temporary1 = QPoint()
