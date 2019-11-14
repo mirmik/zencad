d780f7f9760de1ac79726a16e4d9421e85ad4b6d
mirmik <mirmikns@yandex.ru>
2019-11-14 16:38:29 +0300

    'a'
    Former-commit-id: 3d08fb7346e29363e6053db3590d52d4d44e87db

--- expers/cow.py
+++ expers/cow.py
@@ -28,6 +28,23 @@ class cow(zencad.assemble.unit):
 		def serve(self, delta):
 			pass
 
+
+	class torque_producer(zencad.assemble.unit):
+		def __init__(self, parent, location):
+			super().__init__(parent=parent, location=location)
+			self.set_shape(cylinder(r=1,h=3))
+			self.marshal = vector3(0,0,1)
+			self.signal = 0
+
+		def set_control_signal(self, signal):
+			self.signal = signal
+
+		def get_force_screw(self):
+			return screw(ang=self.marshal * self.signal, lin=(0,0,0))
+
+		def serve(self, delta):
+			pass
+
 	def __init__(self):
 		super().__init__()
 		self.inertia = inertia(1, numpy.diag([1,1,1]))
@@ -38,11 +55,17 @@ class cow(zencad.assemble.unit):
 	def add_force_producer(self, trans):
 		self.force_producer_list.append(self.force_producer(parent=self, location=trans))
 
+	def add_torque_producer(self, trans):
+		self.force_producer_list.append(self.torque_producer(parent=self, location=trans))
+
 	def make_drivers(self):
 		self.force_producer_list = []
 		self.add_force_producer(rotateY(-deg(90)))
 		self.add_force_producer(rotateX(deg(90)))
 		self.add_force_producer(nulltrans())
+		self.add_torque_producer(rotateY(-deg(90)))
+		self.add_torque_producer(rotateX(deg(90)))
+		self.add_torque_producer(nulltrans())
 
 	def serve(self, delta):
 		for f in self.force_producer_list:
@@ -63,15 +86,18 @@ class cow(zencad.assemble.unit):
 		speed_screw_delta_trans = speed_screw_delta.to_trans()
 		self.location = self.location * speed_screw_delta_trans
 
-		self.speed_screw = speed_screw_delta_trans.inverse_rotate(self.speed_screw) 
+		self.speed_screw = self.speed_screw.inverse_transform(speed_screw_delta_trans)
 
-		print(self.location)
+		print(self.speed_screw)
 
 cow = cow()
 
 cow.force_producer_list[0].set_control_signal(1)
-cow.force_producer_list[1].set_control_signal(2)
-cow.force_producer_list[2].set_control_signal(3)
+cow.force_producer_list[1].set_control_signal(0)
+cow.force_producer_list[2].set_control_signal(0)
+cow.force_producer_list[3].set_control_signal(0)
+cow.force_producer_list[4].set_control_signal(0)
+cow.force_producer_list[5].set_control_signal(0.1)
 
 cow.serve(0.1)
 cow.serve(0.1)
