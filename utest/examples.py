4f307259e4fc974c27298c0292da1c9dcf4457b6
mirmik <mirmikns@yandex.ru>
2019-11-16 11:57:22 +0300

    'change example tests'

--- utest/examples.py
+++ utest/examples.py
@@ -3,22 +3,17 @@
 import zencad
 import sys
 import os
+import subprocess
 
 import PyQt5.QtWidgets
 import PyQt5.QtCore
 import PyQt5.QtGui
 
 examples = zencad.util.examples_paths()
-#print(examples)
-
-def exec():
-	pass
-
-PyQt5.QtWidgets.QApplication.exec = exec
 
 for epath in examples:
 	cmd = sys.executable + " -m zencad --disable-show " + epath
 	print(cmd)
-	os.system(cmd)
-
-	pass
+	x = subprocess.check_output(cmd.split())
+	if x != 0:
+		raise Exception("Error in example {0}".format(epath))
