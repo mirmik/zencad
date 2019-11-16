cec1993fa668f75b856a3413d6c806458bd5bfa2
mirmik <mirmikns@yandex.ru>
2019-11-17 02:11:19 +0300

    'disable some examples from travis test'
    Former-commit-id: 01985516777de40c2e851a6e538a3608f48507fc

--- zencad/examples/Embeded/qt_widget.py
+++ zencad/examples/Embeded/qt_widget.py
@@ -10,18 +10,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-try:
-	scn = zencad.Scene()
-except:
-	print("Display missing?")
-	sys.exit(0)
-
+scn = zencad.Scene()
 scn.add(evalcache.unlazy(zencad.box(10)))
 
 app = QApplication(sys.argv[:1])
 zencad.opengl.init_opengl()
 widget = zencad.gui.viewadaptor.DisplayWidget(scn)
 
-if zencad.showapi.SHOWMODE != "noshow":
-	widget.show()
-	app.exec()
+widget.show()
+app.exec()
