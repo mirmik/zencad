e2aee84b418fad8681a2ff6a652f744a967ca4c5
mirmik <mirmikns@yandex.ru>
2020-02-04 02:32:41 +0300

    Add sorting in "sew" algorithm. Add variants of section algorithm. Update manual.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
 0.25.0: Changelog inited. Work on the manual, tetris example now works. 
+0.26.0: Add sorting in "sew" algorithm. Add variants of section algorithm. Update manual. 
--- zencad/geom/boolean.py
+++ zencad/geom/boolean.py
@@ -1,6 +1,6 @@
 import pyservoce
 from zencad.lazifier import lazy, shape_generator
-
+import zencad.transform
 
 @lazy.lazy(cls=shape_generator)
 def union(arr):
@@ -18,5 +18,34 @@ def intersect(arr):
 
 
 @lazy.lazy(cls=shape_generator)
-def section(*args, **kwargs): 
-	return pyservoce.section(*args, **kwargs)
+def section(a, b=0):
+	"""
+		Make section between 'a' and 'b'.
+		Oposite the intersect, which finds the intersection of bodies, 
+		the section finds the intersection of the shells of bodies.
+
+		Arguments:
+		a, b - is pair of algorithm arguments. The algorithm is commutative.
+			a and b can be numeric or vector. In that case algorithm find
+			section with a given plane.
+	"""
+
+	def to_halfspace_if_need(x):
+		if isinstance(x, (tuple, list, pyservoce.vector3)):
+			vec = pyservoce.vector3(x)
+			return (
+				zencad.transform.translate(*vec) * 
+				zencad.transform.short_rotate(fromvec=(0,0,1), tovec=vec)
+			)(pyservoce.halfspace())
+
+		elif isinstance(x, (int, float)):
+			return pyservoce.halfspace().up(x)
+
+		return x
+
+	result = pyservoce.section(
+			to_halfspace_if_need(a),
+			to_halfspace_if_need(b)
+		)
+
+	return result
