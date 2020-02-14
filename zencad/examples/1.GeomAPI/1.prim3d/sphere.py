3518722a3488554dc7168ad8a04397c288640a57
mirmik <mirmikns@yandex.ru>
2020-02-15 00:53:40 +0300

    'pacman'
    Former-commit-id: 834e4b975394663a150ec0d4445a1b0a7ffc2f51

--- zencad/examples/1.GeomAPI/1.prim3d/sphere.py
+++ zencad/examples/1.GeomAPI/1.prim3d/sphere.py
@@ -15,4 +15,16 @@ display(m2.right(30))
 display(m3.right(60))
 display(m4.right(90))
 
+# Pacman
+pacman = (
+	sphere(r=10, yaw=deg(360-45)).rotateY(deg(90)).rotateX(deg(-55))
+	- sphere(r=3).move(5,-7,8)
+	- sphere(r=3).move(-5,-7,8)
+	+ sphere(r=1).move(4,-5,6)
+	+ sphere(r=1).move(-4,-5,6)
+)
+disp(pacman.move(45,50,0), color.yellow)
+disp(sphere(r=2).move(45,35,-2))
+disp(sphere(r=2).move(45,25,-2))
+
 show()
