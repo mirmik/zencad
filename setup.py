7fc854bfe13f5816d60e5197d89e1d79031e70c4
mirmik <mirmikns@yandex.ru>
2020-02-17 21:42:05 +0300

    'window manage'
    Former-commit-id: 5d917282a0d0f510e9385d55bb392db32b947f4a

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if sys.platform != "win32":
 setup(
     name="zencad",
     packages=["zencad"],
-    version="0.28.0",
+    version="0.28.1",
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
