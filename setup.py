6311b80eca96126aa81d0e262779f8fedead4862
mirmik <mirmikns@yandex.ru>
2020-02-22 13:05:47 +0300

    'Add version option'
    Former-commit-id: 20a272b4aae555c0f145f3d8f947c291a13c5792

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ import glob
 import sys
 import os
 
+import zencad.version
+
 directory = os.path.dirname(os.path.realpath(__file__))
 
 requires = [
@@ -26,7 +28,7 @@ if sys.platform != "win32":
 setup(
     name="zencad",
     packages=["zencad"],
-    version="0.28.4",
+    version=zencad.version.__version__,
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
