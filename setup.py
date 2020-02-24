44057bf162e4e4ebe8208e85311317e75747f20f
mirmik <mirmikns@yandex.ru>
2020-02-24 16:39:52 +0300

    'Fix sdist bugs'
    Former-commit-id: 6ce0ad895f2c106eee2a7faed4bff86275bb177a

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if sys.platform != "win32":
 setup(
     name="zencad",
     packages=["zencad"],
-    version="0.28.9",
+    version="0.28.10",
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
