caec6b5dee82fa38a0238c3952cb7e081c884aa0
mirmik <mirmikns@yandex.ru>
2020-02-26 18:27:53 +0300

    'knight'
    Former-commit-id: 887b6d3bdc8065aac02e0207166ee627746a4de7

--- zencad/internal_models/knight.py
+++ zencad/internal_models/knight.py
@@ -3,20 +3,30 @@
 from zencad import * 
 
 def knight():
-	base_h=  3
-	base_r1= 10
-	base_r2= 7
+	base_h=  3 
+	base_r1= 8
+	base_r2= 6
+
+	arrs = 	[
+				[ (-2,2,0), (-2,-2,0), (2,-2,0), (2,2,0) ],
+				[ (-2,2,2), (-1.5,-2.5,2), (1.5,-2.5,2), (2,2,2) ],
+				[ (-3,4,5), (-2,-2,5), (2,-2, 5), (3,4,5) ],
+				[ (-3,5,8), (-2,-1,8), (2,-1,8), (3,5,8) ],
+				[ (-3,4,11), (-2,-1,9), (2,-1,9), (3,4,11) ],
+			]
 
 	m = union(
 		[
 			cylinder(r=base_r1, h=base_h),
 			cone(r1=base_r1, r2=base_r2, h=base_h).up(base_h),
+
 			loft([
-				polysegment([ (-2,2,0), (-2,-2,0), (2,-2,0), (2,2,0) ],closed=True),
-				polysegment([ (-2,2,2), (-2,-2,2), (2,-2,2), (2,2,2) ],closed=True),
-				polysegment([ (-3,4,4), (-2,-2,4), (2,-2,4), (3,4,4) ],closed=True),
-				polysegment([ (-3,4,6), (-2,-2,6), (2,-2,6), (3,4,6) ],closed=True),
-			],smooth=True).up(base_h*2)
+				polysegment(a, closed=True) for a in arrs[:] 
+			],smooth=False).up(base_h*2),
+			
+			#loft([
+			#	polysegment(a, closed=True) for a in arrs[3:]
+			#],smooth=False).up(base_h*2)
 		]
 	)
 
