9905be50533312235daf1476e3311e1383aa5de9
mirmik <mirmikns@yandex.ru>
2020-03-25 18:12:20 +0300

    'a'

--- zencad/convert/svg.py
+++ zencad/convert/svg.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python3
 
+import svgwrite
 
 class SvgWriter:
 	def __init__(self):
+		self.draw =  svgwrite.Drawing()
+
+	def begin(self):
 		pass
+
+	def save(self):
+		self.draw.save()
 
 	def add_edge_to_path(self):
 		pass
 
-	def push_wire(self, wire):
+	def push_edge(self, edge):
 		pass
+
+	def push_wire(self, wire, fill=False):
+		pass
+
+	def push_face(self, face):
+		wire = face.wires()[0]
+		self.push_wire(wire, fill=True)
+
 
 	def push_shape(self, shp):
 		print(shp.curvetype())
 
 		if shp.shapetype() == "face":
-			wire = shp.wires()[0]
-
+			self.push_face(shp)
 
 		elif shp.shapetype() == "edge":
-			if shp.curvetype() == "line":
-				a, b = shp.endpoints()
-				a, b = a[:2], b[:2]
-				self.push_segment(a,b)
+			self.push_edge(shp)
+
+		elif shp.shapetype() == "wire":
+			self.push_wire(shp)
 
 
+def shape_to_svg_string(shape):
+	writer = SvgWriter()
 
+	writer.begin()
+	writer.push_shape(shape)
 
-
+	return writer.draw.tostring()
 
 
 if __name__ == "__main__":
@@ -36,5 +54,4 @@ if __name__ == "__main__":
 	segm = zencad.segment((0,10),(10,0))
 	circ = zencad.circle(10, wire=True)
 
-	shape_to_svg("test0.svg", segm)
-	shape_to_svg("test1.svg", circ)
+	print(shape_to_svg_string(segm))
