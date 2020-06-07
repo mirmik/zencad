b1bed63fd1733dfd3e21efd2e4d9f5045ea95502
netricks <netricks@protonmail.com>
2020-06-08 00:49:13 +0300

    'travis'
    Former-commit-id: 5a9a13d66ac15aa784f34152a764764a137f8a76

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 setup(
     name="zencad",
     packages=["zencad"],
-    version="0.33.0",
+    version="0.33.1",
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
