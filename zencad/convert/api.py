0fa9d1dcd62a931ea0ee8fc5d6147033d3dc79dc
mirmik <mirmikns@yandex.ru>
2021-03-11 00:44:12 +0300

    'comment convert'

--- zencad/convert/api.py
+++ zencad/convert/api.py
@@ -17,26 +17,26 @@ import evalcache
 from zencad.lazifier import lazy
 
 
-@lazy.file_creator(pathfield="path")
-def to_stl(model, path, delta):
-    path = os.path.expanduser(path)
-    pyservoce.make_stl(model, path, delta)
-
-
-@lazy.file_creator(pathfield="path")
-def to_brep(model, path):
-    path = os.path.expanduser(path)
-    pyservoce.brep_write(model, path)
-
-
-def from_brep(path):
-    """Загрузить объект из файла его brep представления."""
-    path = os.path.expanduser(path)
-    f = lazy(lambda p: pyservoce.brep_read(p),
-             hint=str(os.path.getmtime(path)))
-    obj = f(path)
-    evalcache.nocache(obj)
-    return obj
+# @lazy.file_creator(pathfield="path")
+# def to_stl(model, path, delta):
+#    path = os.path.expanduser(path)
+#    pyservoce.make_stl(model, path, delta)
+#
+#
+# @lazy.file_creator(pathfield="path")
+# def to_brep(model, path):
+#    path = os.path.expanduser(path)
+#    pyservoce.brep_write(model, path)
+#
+#
+# def from_brep(path):
+#    """Загрузить объект из файла его brep представления."""
+#    path = os.path.expanduser(path)
+#    f = lazy(lambda p: pyservoce.brep_read(p),
+#             hint=str(os.path.getmtime(path)))
+#    obj = f(path)
+#    evalcache.nocache(obj)
+#    return obj
 
 
 @lazy.file_creator(pathfield="path", prevent_unwrap_in_child=["model"])
