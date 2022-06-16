535b89a29b6750c3120710777401caf2536053e7
mirmik <mirmikns@yandex.ru>
2022-06-17 02:18:56 +0300

    'inc occt version'

--- zencad/version.py
+++ zencad/version.py
@@ -11,5 +11,5 @@ if sys.version_info[1] >= 10:
     __pythonocc_version__ = "7.5.1"
 
 else:
-    __occt_version__ = "7.4.1"
-    __pythonocc_version__ = "7.4.1"
+    __occt_version__ = "7.5.1"
+    __pythonocc_version__ = "7.5.1"
