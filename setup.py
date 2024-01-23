7f8ee34f373b2fd64dba7afc7d5516040fd63c42
mirmik <mirmikns@yandex.ru>
2024-01-23 23:31:37 +0300

    inc version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="zencad",
     packages=["zencad"],
     python_requires='>3.10.0',
-    version="1.3.0",
+    version="1.3.1",
     license="MIT",
     description="CAD system for righteous zen programmers ",
     author="mirmik",
