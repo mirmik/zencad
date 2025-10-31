3218ed42953f48995ad8a3f2a533debd04332b1a
mirmik <mirmikns@yandex.ru>
2025-10-31 18:53:15 +0300

    a

--- expers/30C.py
+++ expers/30C.py
@@ -6,6 +6,8 @@ import time
 import math
 import termin
 import termin.kinematic
+import termin.kinchain
+from termin.algo import linear_solve
 
 tclr = color.transmech
 
@@ -36,6 +38,9 @@ elevation = assemble.unit([
 antenna_link = assemble.unit([])
 antenna = assemble.unit([box([10,30,5], center=True).up(2.5)])
 
+signal_center = assemble.unit(location=up(5))
+signal_center.add_triedron()
+
 azimuth_act.relocate(translate(0,0,0))
 telega_act.relocate(translate(0,0,10))
 slider_act.relocate(translate(0,0,10))
@@ -58,6 +63,7 @@ vslider.link(elevation_act)
 elevation_act.link(elevation)
 elevation.link(antenna_link)
 antenna_link.link(antenna)
+antenna.link(signal_center)
 
 telega_act.set_coord(30)
 slider_act.set_coord(-20)
@@ -65,29 +71,52 @@ kocherga_act.set_coord(deg(45))
 vslider_act.set_coord(25)
 elevation_act.set_coord(deg(45))
 
-# base.set_color(tclr)
-# telega.set_color(tclr)
-# slider.set_color(tclr)
-# kocherga.set_color(tclr)
-# vslider.set_color(tclr)
-
 antenna.set_color(color.red)
-
 interactive = disp(base)
 
+chain = termin.kinchain.KinematicChain3(
+	distal=signal_center.transform,
+	proximal=slider_act.transform)
+
+inited = False
 prevtime = time.time()
 def animate(wdg):
+	global inited
+	global prevtime
+	if not inited:
+		inited = True
+		prevtime = time.time()
+		return
+
 	delta = (time.time() - prevtime)
-	azimuth_act.set_coord(deg(30) * math.sin(time.time()))
-	telega_act.set_coord(30 * math.sin(time.time()))
-	slider_act.set_coord(-20 + -10 * math.sin(time.time() * 1.5))
-	kocherga_act.set_coord(deg(45 * math.sin(time.time() * 0.7)))
-	vslider_act.set_coord(24 - 10 * math.sin(time.time() * 0.7))
-	elevation_act.set_coord(deg(45 * math.sin(time.time() * 0.7)))
+	#azimuth_act.set_coord(deg(30) * math.sin(time.time()))
+	#telega_act.set_coord(30 * math.sin(time.time()))
+	#slider_act.set_coord(-20 + -10 * math.sin(time.time() * 1.5))
+	#kocherga_act.set_coord(deg(45 * math.sin(time.time() * 0.7)))
+	#vslider_act.set_coord(24 - 10 * math.sin(time.time() * 0.7))
+	#elevation_act.set_coord(deg(45 * math.sin(time.time() * 0.7)))
+
+	J0 = chain.sensitivity_jacobian()
+
+	#U = numpy.array([0,0,0,0,-0.1,0])
+	W = numpy.array([-0.01,0,0,0,0,0])
+	
+	Ut = (
+		+ numpy.array([0,0,55]) 
+		- signal_center.transform.global_pose().lin
+	) * 0.01
+	U = numpy.array([-0.01,0,0,Ut[0],Ut[1],Ut[2]])
+
+	A = J0.T.dot(J0)
+	b = J0.T.dot(U)
+
+	x = linear_solve(A,b)
+	#print(x)
+
+	chain.apply_coordinate_changes(x)
+	base.update_location_from_transform()
 
 	interactive.location_update()
-
-	termin.transform.inspect_tree(base.transform, name_only=True)
-	raise Exception("Stop")
+	#raise Exception("Stop")
 
 show(animate=animate)
